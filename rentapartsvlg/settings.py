c38e95e2da7d979c19a7c294a5df03e5249c02d2
Artem <Gakerchannel@gmail.com>
2024-08-01 14:47:39 +0300

    postgres ready

--- rentapartsvlg/settings.py
+++ rentapartsvlg/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
 
     "debug_toolbar",
 
@@ -83,8 +84,12 @@ WSGI_APPLICATION = 'rentapartsvlg.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rentvlg',
+        'USER': 'rentvlg',
+        'PASSWORD': 'rentvlg',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
